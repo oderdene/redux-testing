4ce0a48378baea2efd92a2b22da4fd9b13e8ba06
oderdene <oderdene.ch@gmail.com>
2018-04-03 13:42:48 +0800

    test section 2

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,9 +1,19 @@
 import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-// use describe to group together similar tests
-describe('App')
+// use 'describe' to group together similar tests
+// App is just a string
+describe('App', () => {
 
-it
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
 
-expect
+    // create an instance of App
+    const component = renderComponent(App);
+
+    // Use 'expect' to make an 'assertion' about a target
+    expect(component).to.contain('React simple starter');
+
+  });
+
+});
