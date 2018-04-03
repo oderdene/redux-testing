781e2c04fd8f0de24c0b84ff9f93ee25658121d5
oderdene <oderdene.ch@gmail.com>
2018-04-03 10:22:26 +0800

    first commit

---
+++ test/components/app_test.js
@@ -0,0 +1,9 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+// use describe to group together similar tests
+describe('App')
+
+it
+
+expect
